5f1d5f006e6ef9187472415ad34260733d455c31
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2016-11-14 17:12:37 +0000

    create class autoloader

---
+++ autoload.php
@@ -0,0 +1,9 @@
+<?php
+spl_autoload_register('class_autoloader');
+
+function class_autoloader($class) {
+    $file = __DIR__ . '/classes/' . $class . '.php';
+    if (is_readable($file)) {
+        include $file;
+    }
+}
