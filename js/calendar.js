56d7673eada0ca8f66e7d2757d9bf91e06dde369
Rhymes <rhymes@triplejdeveloping.com>
2016-11-14 20:36:03 +0000

    Amended Calendar Cunctionality.
    Changed datepicker.min.js output to Uk format DDMMYYYY.
    Set from calendar to default to current month.
    Set to calendar to default to next month.
    Added multiple selection function to both calendars.
    Added a clear button to remove all selections in each calendar.

--- js/calendar.js
+++ js/calendar.js
@@ -1,9 +1,32 @@
 $(function() {
 
-    $('.datepicker').datepicker();
-    $('#fromCal').on("changeDate", function() {
-        $('#fromDate').text(
-            $('#fromCal').datepicker('getFormattedDate')
+    $date = new Date();
+    $month = $date.getMonth();
+
+    $fromDate = $('#fromCal')
+    $toDate = $('#toCal')
+
+    $fromDate.datepicker({
+        multidate: true,
+        todayHighlight: true,
+        clearBtn: true,
+        defaultViewDate: { month: $month }});
+
+    $fromDate.on("changeDate", function() {
+        $('#fromSpan').text(
+            $fromDate.datepicker('getFormattedDate')
+        );
+    });
+
+    $toDate.datepicker({
+        multidate: true,
+        todayHighlight: true,
+        clearBtn: true,
+        defaultViewDate: { month: $month + 1 }});
+
+    $toDate.on("changeDate", function() {
+        $('#toSpan').text(
+            $toDate.datepicker('getFormattedDate')
         );
     });
 
