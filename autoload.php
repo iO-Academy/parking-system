9780ba404b48a49e50db665c85d646150591bef6
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2016-11-14 17:13:30 +0000

    change autoloader to include once

--- autoload.php
+++ autoload.php
@@ -4,6 +4,6 @@ spl_autoload_register('class_autoloader');
 function class_autoloader($class) {
     $file = __DIR__ . '/classes/' . $class . '.php';
     if (is_readable($file)) {
-        include $file;
+        include_once $file;
     }
 }
