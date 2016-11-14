d4eec0138efbae1ee103703c19eebc603b51eda7
Luke Winsley <lukewinsley@gmail.com>
2016-11-14 17:05:11 +0000

    created login function in user class

--- classes/user.php
+++ classes/user.php
@@ -1,8 +1,26 @@
 <?php
 
-class user {
+class User {
 
-    //public function login(){}
+    //login function
+    function login($database, $email, $password) {
+
+        $sql = "SELECT * FROM `users` WHERE `email` = :email;";
+        $query = $database->prepare($sql);
+        $query->execute([':email'=>$email]);
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+
+        $encryptPass = $user["id"] . $password;
+        $encryptPass = sha1($encryptPass);
+
+        if(empty($user)) {
+            //throw error = user does not exist
+        } elseif($user["password"] != $encryptPass) {
+            //throw error = incorrect email and password combination
+        } else {
+            //login
+        }
+    }
 
     //public function logout(){}
 
