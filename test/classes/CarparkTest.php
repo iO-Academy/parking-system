74b4c6b5b051a63656a81276edea7ccd8705f044
Ben Morris <ben.morris@mayden.co.uk>
2016-11-15 14:44:50 +0000

    Create unit tests for Carpark class WORKING

--- test/classes/CarparkTest.php
+++ test/classes/CarparkTest.php
@@ -13,7 +13,40 @@ use PHPUnit\Framework\TestCase;
  */
 class CarparkTest extends TestCase
 {
-    public function testConstructor() {
+    public function testSuccessfulInstantiation() {
+
+        $mockPDO = $this->createMock('PDO');
+        $mockStatement = $this->createMock('PDOStatement');
+        $mockStatement->method('fetch')->willReturn([
+            'id' => '1',
+            'name' => 'test_name',
+            'capacity' => 'test_capacity',
+            'isVisitor' => 1
+        ]);
+        $mockPDO->method('prepare')->willReturn($mockStatement);
+
+        $failed = false;
+        try {
+            $user = new User($mockPDO, 'test_name');
+        } catch (Exception $e) {
+            $failed = true;
+        }
+        $this->assertFalse($failed);
 
     }
+
+    public function testFailedInstantiation() {
+        $mockPDO = $this->createMock('PDO');
+        $mockStatement = $this->createMock('PDOStatement');
+        $mockStatement->method('fetch')->willReturn([]);
+        $mockPDO->method('prepare')->willReturn($mockStatement);
+
+        try {
+            $user = new User($mockPDO, 'test_name');
+        } catch (Exception $e) {
+            
+        }
+
+    }
+
 }
