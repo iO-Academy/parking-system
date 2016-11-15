a4c19598c426b2a6cfcc35e5fa93ab92664b6a1c
Luke Winsley <lukewinsley@gmail.com>
2016-11-15 17:07:22 +0000

    end of day commit

---
+++ js/account-page.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('#edit').click(function (){
+        $('.user-account-content').toggle()
+    })
+
+    $('#save-user-details').click(function (){
+        $('.user-account-content').toggle()
+    })
+})
