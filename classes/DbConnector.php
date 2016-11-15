29c4028728b59bd2736561fe92c445af3038f719
Luke Winsley <lukewinsley@gmail.com>
2016-11-15 09:24:43 +0000

    declare params inside of construct

--- classes/DbConnector.php
+++ classes/DbConnector.php
@@ -5,8 +5,13 @@ class DbConnector
 
     private $conn;
 
-    public function __construct($servername, $dbname, $username, $password)
+    public function __construct()
     {
+
+        $servername = "192.168.20.56";
+        $dbname = "parkingSystem";
+        $username = "root";
+        $password = "";
 
         try {
             $this->conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
