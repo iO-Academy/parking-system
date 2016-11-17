bfb2de10532a7e487929852891cf241a51e0ce04
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-17 14:14:28 +0000

    created time and date variables, and dateConvert()

--- js/book.js
+++ js/book.js
@@ -1,13 +1,30 @@
 $(function() {
 
+    // getting visitor selected date and times into a 'to' and 'from' datetime variable
+    var vd = $('#visitorCal').datepicker('getFormattedDate'), // visitor date
+        vft = $('#fromHours').val() + ':' + $('#fromMinutes').val(), // visitor from time
+        vtt = $('#toHours').val() + ':' + $('#toMinutes').val(), // visitor to time
+        fdt, // from date time
+        tdt // to date time
+
+    function dateConvert(date) {
+        return date.split('/').reverse().join('-')
+    }
+
+
+
+
     /**
      * DOCBLOCK ME!
      */
     $("#visitorSubmit").click(function() {
 
+        var x //change var name
+        x = $('#visitorCal').datepicker('getFormattedDate')
+
         var data = {
-            'from': $('#fromSpan').text(),
-            'to': $('#toSpan').text(),
+            from:
+
         }
 
         $.ajax({
