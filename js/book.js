9b0f101d0e0a858a6d70b817048790f98d7c4a81
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-17 16:01:26 +0000

    finished writing createAjaxData()

--- js/book.js
+++ js/book.js
@@ -1,13 +1,18 @@
 $(function() {
 
+
     // DOBCLOCK ME
     function dateConvert(date) {
         return date.split('/').reverse().join('-')
     }
+
+
     // DOBCLOCK ME
     function changeToDateTime(date, time) {
         return dateConvert(date) + ' ' + time + ':00'
     }
+
+
     // DOBCLOCK ME
     function makeBooking(data) {
         $.ajax({
@@ -18,31 +23,36 @@ $(function() {
         })
     }
 
+
+    // DOCBLOCK ME
     // getting selected date(s) (and times) into datetime format and putting into ajax data object, along with carParkId
     function createAjaxData(carParkId) {
 
         var vd, // visitor date
             vft, // visitor from-time
             vtt, // visitor to-time
+
             sfd, // staff from-date
             std, // staff to-date
+
             fdt, // from-datetime
             tdt, // to-datetime
+
             data
 
         if (carParkId == 4) { // getting visitor selected date and times into a 'to' and 'from' datetime variable
 
-            var vd = $('#visitorCal').datepicker('getFormattedDate'),
-                vft = $('#fromHours').val() + ':' + $('#fromMinutes').val(),
-                vtt = $('#toHours').val() + ':' + $('#toMinutes').val(),
+            vd = $('#visitorCal').datepicker('getFormattedDate')
+            vft = $('#fromHours').val() + ':' + $('#fromMinutes').val()
+            vtt = $('#toHours').val() + ':' + $('#toMinutes').val()
 
             fdt = changeToDateTime(vd, vft)
             tdt = changeToDateTime(vd, vtt)
 
         } else { // getting staff selected date and times into a 'to' and 'from' datetime variable
 
-            var sfd = $('#fromCal').datepicker('getFormattedDate'),
-                std = $('#toCal').datepicker('getFormattedDate'),
+            sfd = $('#fromCal').datepicker('getFormattedDate')
+            std = $('#toCal').datepicker('getFormattedDate')
 
             fdt = changeToDateTime(sfd, "00:00")
             tdt = changeToDateTime(std, "23:59")
@@ -55,73 +65,32 @@ $(function() {
             toDateTime: tdt
         }
 
+        return data
+
 
     }
 
-    // sends ajax request when carpark4 (visitor/rich tea) book button is clicked
+
+    // sends ajax request when carpark4 (visitor:rich tea) book button is clicked
     $('#carpark4').click(function () {
 
-        // getting visitor selected date and times into a 'to' and 'from' datetime variable
-        var vd = $('#visitorCal').datepicker('getFormattedDate'), // visitor date
-            vft = $('#fromHours').val() + ':' + $('#fromMinutes').val(), // visitor from-time
-            vtt = $('#toHours').val() + ':' + $('#toMinutes').val(), // visitor to-time
-            fdt, // from-datetime
-            tdt, // to-datetime
-            data
-
-        fdt = changeToDateTime(vd, vft)
-        tdt = changeToDateTime(vd, vtt)
-
-        data = {
-            carPark: 4,
-            fromDateTime: fdt,
-            toDateTime: tdt
-        }
-
+       createAjaxData(4)
         makeBooking(data)
     })
 
-    // sends ajax request when staff carpark1 (hobnob) book button is clicked
+
+    // sends ajax request when staff carpark1 (staff:hobnob) book button is clicked
     $('#carpark1').click(function () {
 
-        // getting staff selected date and times into a 'to' and 'from' datetime variable
-        var sfd = $('#fromCal').datepicker('getFormattedDate'), // staff from-date
-            std = $('#toCal').datepicker('getFormattedDate'), // staff to-date
-            fdt, // from-datetime
-            tdt, // to-datetime
-            data
-
-        fdt = changeToDateTime(sfd, "00:00")
-        tdt = changeToDateTime(std, "23:59")
-
-        data = {
-            carPark: 1,
-            fromDateTime: fdt,
-            toDateTime: tdt
-        }
-
+        createAjaxData(1)
         makeBooking(data)
     })
 
-    // sends ajax request when staff carpark3 (digestive) book button is clicked
+
+    // sends ajax request when staff carpark3 (staff:digestive) book button is clicked
     $('#carpark3').click(function () {
 
-        // getting staff selected date and times into a 'to' and 'from' datetime variable
-        var sfd = $('#fromCal').datepicker('getFormattedDate'), // staff from-date
-            std = $('#toCal').datepicker('getFormattedDate'), // staff to-date
-            fdt, // from-datetime
-            tdt, // to-datetime
-            data
-
-        fdt = changeToDateTime(sfd, "00:00")
-        tdt = changeToDateTime(std, "23:59")
-
-        data = {
-            carPark: 3,
-            fromDateTime: fdt,
-            toDateTime: tdt
-        }
-
+        createAjaxData(3)
         makeBooking(data)
     })
 
