6e62f6217821974708eef529b841aa5fa442dd67
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-17 14:39:10 +0000

    wrote toDateTime() and data object

--- js/book.js
+++ js/book.js
@@ -1,37 +1,40 @@
 $(function() {
 
     // getting visitor selected date and times into a 'to' and 'from' datetime variable
-    var vd = $('#visitorCal').datepicker('getFormattedDate'), // visitor date
-        vft = $('#fromHours').val() + ':' + $('#fromMinutes').val(), // visitor from time
-        vtt = $('#toHours').val() + ':' + $('#toMinutes').val(), // visitor to time
+    var vd = 'DD/MM/YYYY',//$('#visitorCal').datepicker('getFormattedDate'), // visitor date
+        vft = '11:11',//$('#fromHours').val() + ':' + $('#fromMinutes').val(), // visitor from time
+        vtt = '22:22',//$('#toHours').val() + ':' + $('#toMinutes').val(), // visitor to time
         fdt, // from date time
-        tdt // to date time
+        tdt, // to date time
+        data
 
     function dateConvert(date) {
         return date.split('/').reverse().join('-')
     }
 
+    function toDateTime(date, time) {
+        return dateConvert(date) + ' ' + time + ':00'
+    }
 
+    fdt = toDateTime(vd, vft)
+    tdt = toDateTime(vd, vtt)
 
+    data = {
+        carPark: 'visitor', //needs to be a variable (or a different literal for staff)
+        fromDateTime: fdt,
+        toDateTime: tdt
+    }
 
-    /**
-     * DOCBLOCK ME!
-     */
-    $("#visitorSubmit").click(function() {
+    console.log(data)
 
-        var x //change var name
-        x = $('#visitorCal').datepicker('getFormattedDate')
-
-        var data = {
-            from:
-
-        }
+    function makeBooking(data) {
 
         $.ajax({
             method: "POST",
             url: "../ajax/createBooking.php",
             data: data,
-
+            success: function() {} // need to talk with Ali about making his modal indicate success
         })
-    })
+    }
+
 })
