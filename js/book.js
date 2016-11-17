75afe527c616324757efc1c03ebc0074e10fd0ff
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-17 15:49:14 +0000

    started defining createAjaxData()

--- js/book.js
+++ js/book.js
@@ -18,6 +18,46 @@ $(function() {
         })
     }
 
+    // getting selected date(s) (and times) into datetime format and putting into ajax data object, along with carParkId
+    function createAjaxData(carParkId) {
+
+        var vd, // visitor date
+            vft, // visitor from-time
+            vtt, // visitor to-time
+            sfd, // staff from-date
+            std, // staff to-date
+            fdt, // from-datetime
+            tdt, // to-datetime
+            data
+
+        if (carParkId == 4) { // getting visitor selected date and times into a 'to' and 'from' datetime variable
+
+            var vd = $('#visitorCal').datepicker('getFormattedDate'),
+                vft = $('#fromHours').val() + ':' + $('#fromMinutes').val(),
+                vtt = $('#toHours').val() + ':' + $('#toMinutes').val(),
+
+            fdt = changeToDateTime(vd, vft)
+            tdt = changeToDateTime(vd, vtt)
+
+        } else { // getting staff selected date and times into a 'to' and 'from' datetime variable
+
+            var sfd = $('#fromCal').datepicker('getFormattedDate'),
+                std = $('#toCal').datepicker('getFormattedDate'),
+
+            fdt = changeToDateTime(sfd, "00:00")
+            tdt = changeToDateTime(std, "23:59")
+
+        }
+
+        data = {
+            carPark: carParkId,
+            fromDateTime: fdt,
+            toDateTime: tdt
+        }
+
+
+    }
+
     // sends ajax request when carpark4 (visitor/rich tea) book button is clicked
     $('#carpark4').click(function () {
 
