838a7e439b4b26284bee68cfbb4f5a41bbdeaa2f
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-17 15:03:26 +0000

    wrote visitor booking button click function and started staff1 version

--- js/book.js
+++ js/book.js
@@ -1,34 +1,15 @@
 $(function() {
 
-    // getting visitor selected date and times into a 'to' and 'from' datetime variable
-    var vd = 'DD/MM/YYYY',//$('#visitorCal').datepicker('getFormattedDate'), // visitor date
-        vft = '11:11',//$('#fromHours').val() + ':' + $('#fromMinutes').val(), // visitor from time
-        vtt = '22:22',//$('#toHours').val() + ':' + $('#toMinutes').val(), // visitor to time
-        fdt, // from date time
-        tdt, // to date time
-        data
-
+    // DOBCLOCK ME
     function dateConvert(date) {
         return date.split('/').reverse().join('-')
     }
-
+    // DOBCLOCK ME
     function toDateTime(date, time) {
         return dateConvert(date) + ' ' + time + ':00'
     }
-
-    fdt = toDateTime(vd, vft)
-    tdt = toDateTime(vd, vtt)
-
-    data = {
-        carPark: 'visitor', //needs to be a variable (or a different literal for staff)
-        fromDateTime: fdt,
-        toDateTime: tdt
-    }
-
-    console.log(data)
-
+    // DOBCLOCK ME
     function makeBooking(data) {
-
         $.ajax({
             method: "POST",
             url: "../ajax/createBooking.php",
@@ -37,4 +18,50 @@ $(function() {
         })
     }
 
+    // sends ajax request when visitor carpark book button is clicked
+    $('#carpark4').click(function () {
+
+        // getting visitor selected date and times into a 'to' and 'from' datetime variable
+        var vd = $('#visitorCal').datepicker('getFormattedDate'), // visitor date
+            vft = $('#fromHours').val() + ':' + $('#fromMinutes').val(), // visitor from-time
+            vtt = $('#toHours').val() + ':' + $('#toMinutes').val(), // visitor to-time
+            fdt, // from-datetime
+            tdt, // to-datetime
+            data
+
+        fdt = toDateTime(vd, vft)
+        tdt = toDateTime(vd, vtt)
+
+        data = {
+            carPark: 'visitor', //needs to be a variable (or a different literal for staff)
+            fromDateTime: fdt,
+            toDateTime: tdt
+        }
+
+        makeBooking(data)
+    })
+
+    // sends ajax request when staff carpark hobnob book button is clicked
+    $('#carpark1').click(function () {
+
+        // getting staff selected date and times into a 'to' and 'from' datetime variable
+        var sfd = $('#fromCal').datepicker('getFormattedDate'), // staff from-date
+            std = $('#toCal').datepicker('getFormattedDate'), // staff to-date
+            fdt, // from-datetime
+            tdt, // to-datetime
+            data
+
+        fdt = toDateTime(vd, vft)
+        tdt = toDateTime(vd, vtt)
+
+        data = {
+            carPark: 'visitor', //needs to be a variable (or a different literal for staff)
+            fromDateTime: fdt,
+            toDateTime: tdt
+        }
+
+        makeBooking(data)
+    })
+
 })
+
